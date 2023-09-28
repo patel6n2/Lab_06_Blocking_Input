e50ab3ead16fcf07b18c0e0fe46fb1acdc76649a
Nishaan Patel <patel6n2@mail.uc.edu>
2023-09-28 15:18:25 -0400

    Task 1 Added

--- src/CToFConverter.java
+++ src/CToFConverter.java
@@ -1,7 +1,31 @@
+import java.util.Scanner;
+
 public class CToFConverter
 {
     public static void main(String[] args)
     {
-
+        Scanner in = new Scanner(System.in);
+        double celsius = 0;
+        double fahrenheit = 0;
+        String trash = "";
+        boolean done = false;
+        do
+        {
+            System.out.print("Enter degrees in C: ");
+            if(in.hasNextDouble())
+            {
+                celsius = in.nextDouble();
+                in.nextLine();
+                fahrenheit = (9.0 / 5.0 * celsius) + 32;
+                System.out.println(celsius + " degrees C is " + fahrenheit + " degrees F");
+                done = true;
+            }
+            else
+            {
+                trash = in.nextLine();
+                System.out.println("You said the temperature is " + trash);
+                System.out.println("You must enter valid input values.");
+            }
+        } while (!done);
     }
 }
